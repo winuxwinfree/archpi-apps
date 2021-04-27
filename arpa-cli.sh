5f258461861e8bec21819fd6d6a76aebd011abc2
winuxwinfree <76043740+winuxwinfree@users.noreply.github.com>
2021-04-27 11:23:12 +0200

    Update arpa-cli.sh

--- arpa-cli.sh
+++ arpa-cli.sh
@@ -1,299 +1,10 @@
 #!/bin/bash
 #AUTHOR: androrama, fenixlinux.com
 #License GPLV3
-#Script inspired by Fenix Updater and Fenix Assistant 
 
 clear
-cd
-rm -f updater.sh*
-
-#MFjaro patch function.
-
-patch () {
-  echo 
-  echo "Apply this patch if you have any of the following problems:"
-  echo "1: Crackling, popping, and other sound problems."
-  echo "   (After this the equalizer and pulse-audio options will not work)."
-  echo "2: Bluetooth service does't work."
-  echo "3: Discord does't work."
-  echo "4: The start-up sound doesn't work."
-  echo 
-  echo "In addition, it will do the following:"
-  echo "1: Install raspi-config."
-  echo "2: Replace Pantheon-screenshot by Gnome-screenshot."
-  echo "3: Ask if you want to add more software."
-  echo 
-
-  while true; do
-    read -p "Run this patch? [y/n]: " yn
-  case $yn in
-    [Yy]* ) 
-
-
-  echo ""
-  
-   
-  #alsa audio fix
-  
-  echo "Step 1, installing alsa."
-  
-  DIRECTORY=/usr/bin
-  FILE=/usr/bin/qasmixer 
-  
-  if [ ! -f "$FILE" ]; then
-      read -p " Alsa sound is better, but pulseaudio equalizer will not work, continue? (y/n)]=> " answer 
-  if [ $answer = y ] || [ $answer = Y ]; then
-    pkill pulseaudio || echo "Error killing pulseaudio, maybe it's killed."
-    systemctl --user mask pulseaudio.service || echo "Error masking pulseaudio.service, maybe it's already masked."
-    systemctl --user mask pulseaudio.socket || echo "Error masking pulseaudio.socket, maybe it's already masked."
-    sudo pacman -S alsa-utils || echo "Error installing alsa-utils."
-    sudo pacman -S qastools || echo "Error installing qastools."
-    
-    FILE=$HOME/Desktop/qasmixer.desktop
-    if [ ! -f "$FILE" ]; then
-    cp /usr/share/applications/qasmixer.desktop $HOME/Desktop/ || echo "Error creating qasmixer shortcut on desktop"
-    fi
-    
-    echo
-    echo
-    echo "If you switched pulse to alsa follow the steps below to configure it correctly:
-   1. Right click on the volume icon.
-   2. Go to volume control settings.
-   3. Where it says -Command to open the mixer- type -alsamixer-. 
-      (you can also use this command in a terminal). 
-   Now you can open the advanced sound control settings 
-   by clicking on -Launch Mixer-.
-   4. Reboot.
-
-   With raspi-config you can change the 
-   audio output from hdmi to jack and viceversa.
-   " >> $HOME/Desktop/AudioFix.txt
-   echo
-   echo
-    echo -e "To complete the configuration read the AudioFix.txt\nfile that has been created on the desktop."
-   echo
-   for i in {0..10..1}; do echo -e "$i"'\c'
-   echo -n " "
-   sleep 1
-   done
-   echo
-   echo
-
-  else
-    echo "Unable to install alsa."
-  fi
-  
- else
-    echo "Nothing to do."
- fi
-  
-  #bluetooth fix
-  
-  echo "Step 2, repairing bluetooth problem."
-  
-  STRING="kgdboc=serial0"
-  FILE="/boot/cmdline.txt"
-  
-  if grep -qF "$STRING" "$FILE";then
-   echo "Nothing to do."
-  else
-   sudo systemctl unmask attach-bluetooth.service;
-   sudo systemctl start attach-bluetooth.service;
-   sudo sed -i 's/kgdboc=ttyAMA0/kgdboc=serial0/g' /boot/cmdline.txt || echo "Error replacing ttyAMAO with serial0 in /boot/cmdline."
-   sudo sed -i 's/console=ttyAMA0/console=serial0/g' /boot/cmdline.txt || echo "Error replacing ttyAMAO with serial0 in /boot/cmdline."
-   echo "Done." 
-  fi
-  
-  #discord fix
-  
-  echo "Step 3, repairing discord.";
-  DIRECTORY=/usr/share/bin
-  FILE=/usr/bin/discord
-  if [ ! -d "$DIRECTORY" ]; then
-    sudo mkdir /usr/share/bin
-  elif [[ -d "$DIRECTORY" && -f "$FILE" ]]; then 
-    sudo mv /usr/bin/discord /usr/share/bin/ || echo "Error installing Discord."
-    echo "Done."
-  else
-    echo "Nothing to do."
-  fi
-
-  FILE=$HOME/Desktop/updater.desktop
-  if [ ! -f "$FILE" ]; then
-   cp /usr/share/applications/updater.desktop $HOME/Desktop/ || echo "Error creating mfjupdater shortcut on desktop, maybe it already exists.";
-  fi
-  
-  
-  #xp alsa boot sound fix
-  
-  echo "Step 4, repairing the start-up sound.";
-  
-  echo "@cvlc --play-and-exit /home/pi/xp.ogg" >> $HOME/.config/lxsession/LXDE/autostart 
-  
-  echo "Done."
-  
-  
-  #config.txt editable from home directory.
-  
-  echo "Step 5, Creating a config.txt editable from home directory."
  
-  FILE=$HOME/config.txt
-  if [ ! -x "$FILE" ]; then
-  echo -n "#!" > ~/config.txt
-  echo -n "/bin/bash" >> ~/config.txt
-  echo -e "\nlxterminal -e sudo nano /boot/config.txt" >> config.txt
-  sudo chmod +x $HOME/config.txt || echo "Error"
-  echo "Done."
-  else
-    echo "Nothing to do."
-  fi
-  
-  
-  #install raspi-config
-    
-  echo "Step 6, installing raspi-config.";
-  
-  FILE=/usr/bin/raspi-config
-  if [ ! -f "$FILE" ]; then
-  
-  rm ~/.local/share/applications/raspi-config.desktop
-  
-  wget --continue https://sourceforge.net/projects/fenixlinux/files/repo/archlinux/pi/raspi-config-git.pkg.tar.zst
-  
-  sudo pacman -U raspi-config-git.pkg.tar.zst 
-  
-  echo "[Desktop Entry]
-  Type=Application
-  Name=Raspi-config
-  GenericName=Raspi-config
-  Comment=Raspi-config port for archlinux and MfJaro
-  Categories=System;
-  Exec=lxterminal -e sudo raspi-config
-  Icon=/usr/share/icons/hicolor/128x128/apps/rpi-imager.png
-  StartupWMClass=Tor Browser" > ~/.local/share/applications/raspi-config.desktop
-  
-  echo "Done.";
-  
-  else
-    echo "Nothing to do."
-  fi
 
-  #Replace Pantheon-screenshot by Gnome-screenshot.
-  
-  echo "Step 7, replacing Pantheon-screenshot by Gnome-screenshot.";
-  
-  FILE=/var/lib/pacman/local/pantheon-screenshot*
-  if [ -f "$FILE" ]; then
-  read -p "Replace Pantheon-screenshot by Gnome-screenshot? (y/n)]=> " answer 
- 
-   if [ $answer = y ] || [ $answer = Y ]; then
-      sudo pacman -R pantheon-screenshot
-      sudo pacman -S gnome-screenshot
-   fi
-  else
-    echo "Nothing to do."
-  fi
-  
-  sleep 3;
-   
-  echo
-  read -p "Run the Add-On Wizard? [y/n]=> " answer
-  if [ $answer = y ] || [ $answer = Y ]; then
-   addapps;
-   else 
-   clear
-   menu
-  fi
-  
-  sleep 3;
-  
-   exit 1;;
-
-           [Nn]* )  clear; menu;;
-           * ) echo "Please answer yes or no.";;
-       esac
-   done
- 
-}
-
- #Install Tor-Browser function / Thanks to botspot.
-
- tor () {
-
- cd
- DIRECTORY="$(dirname "$(dirname "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")")"
-
- function error {
-   echo -e "\\e[91m$1\\e[39m"
-   exit 1
- }
-
- rm -r ~/tor-browser_en-US 
- rm ~/tor.tar.xz
- rm ~/.local/share/applications/tor.desktop
-
- wget https://sourceforge.net/projects/tor-browser-ports/files/10.0.14-arm64-alsa/tor-browser-linux-arm64-10.0.14_en-US.tar.xz || error "Failed to download!"
-
- tar -xf ~/tor-browser-linux-arm64-10.0.14_en-US.tar.xz || error "Failed to extract!"
-
- echo "[Desktop Entry]
- Type=Application
- Name=Tor Browser
- GenericName=Web Browser
- Comment=Tor Browser is +1 for privacy and −1 for mass surveillance
- Categories=Network;WebBrowser;Security;
- Exec=$HOME/tor-browser_en-US/Browser/start-tor-browser
- X-TorBrowser-ExecShell=./Browser/start-tor-browser --detach
- Icon=/usr/share/icons/tor.png
- StartupWMClass=Tor Browser" > ~/.local/share/applications/tor.desktop
-
- rm $HOME/.local/share/applications/torinstall.desktop
- xdg-open https://sourceforge.net/projects/tor-browser-ports/files/ >> /dev/null
- echo Done
- sleep 10
-
-}
-
-#Download wine apps and games function.
-
-wineapps () {
-
-if [ -d "./wineapps" ] 
-then
-    cd wineapps
-    git pull
-else
-    git clone https://github.com/winuxwinfree/wineapps
-    
-fi
-echo "Open it from wine explorer. Wine explorer path: My Documents/wineapps."
-sleep 10
-}
-
-#Pulseaudio/alsa(better sound) switcher function.
-
-audiofix () {
-
-echo "";
-read -p "Type (p) to use pulseaudio or (a) to use alsa=> " answer
-if [ $answer = p ] || [ $answer = pulseaudio ]; then
-  echo "Switching to pulseadio."
-  systemctl --user unmask pulseaudio.service || echo "Error unmasking pulseaudio.service, maybe it's already unmasked."
-  systemctl --user unmask pulseaudio.socket || echo "Error unmasking pulseaudio.socket, maybe it's already unmasked."
-  echo "Done, you can close this window."
-  echo "Reboot the system to apply the changes."
-  sleep 20
-elif [ $answer = a ] || [ $answer = alsa ]; then
-  echo "Switching to alsa. The pulseadio equalizer will stop working."
-  pkill pulseaudio
-  systemctl --user mask pulseaudio.service || echo "Error masking pulseaudio.service, maybe it's already masked."
-  systemctl --user mask pulseaudio.socket || echo "Error masking pulseaudio.socket, maybe it's already masked."
-  echo -e "If you switched pulse to alsa follow the steps below to configure it correctly: \n 1. Reboot. \n 2. Right click on the volume icon. \n 3. Go to volume control settings. \n 4. Where it says -Command to open the mixer- type -alsamixer-. \n (you can also use this command in a terminal). \n Now you can open the advanced sound control settings \n by clicking on -Launch Mixer-."
-  echo "Done, you can close this window."
-  sleep 999
-fi
-
-}
 
 #Add software function.
 
@@ -1354,134 +1065,55 @@ fi
 
 }
 
-menu () {
+ #Install Tor-Browser function / Thanks to botspot.
 
-#Fenix Updater -cli version- main menu.
+ tor () {
 
-while :
-  do
+ cd
+ DIRECTORY="$(dirname "$(dirname "$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )")")"
 
+ rm -r ~/tor-browser_en-US 
+ rm ~/tor.tar.xz
+ rm ~/.local/share/applications/tor.desktop
 
-	echo "   __     __)            __     __)                     
-  (, /|  /|    /)  ,    (, /   /        /)              
-    / | / |   //          /   /  __   _(/ _  _/_  _  __ 
- ) /  |/  |_ /(_  /_...  /   /   /_)_(_(_(_(_(___(/_/ (_
-(_/   '     /) .-/      (___(_.-/     Cli version                  
-           (/ (_/            (_/ 
----------------------------------------------------------"
-echo "1) Upgrade the system. "
-echo "2) Clean unused packages and cache."
+ wget https://sourceforge.net/projects/tor-browser-ports/files/10.0.14-arm64-alsa/tor-browser-linux-arm64-10.0.14_en-US.tar.xz || error "Failed to download!"
 
+ tar -xf ~/tor-browser-linux-arm64-10.0.14_en-US.tar.xz || error "Failed to extract!"
 
-echo " __                                                 
-|__)_ |_ _|_  _ _   _  _  _|   _  _| _| __  _  _  _ 
-|  (_||_(_| )(-_)  (_|| )(_|  (_|(_|(_|    (_)| )_) 
----------------------------------------------------------       "
-echo "a) Repair the most common problems."
-echo "b) Download wine apps and games."
-echo "c) Pulseaudio/alsa(better sound) switcher."
-echo "d) Add-ons."
-echo ""
-echo -n "[Type an option: 1,2,a,b,c,d and then press INTRO]=> "
+ echo "[Desktop Entry]
+ Type=Application
+ Name=Tor Browser
+ GenericName=Web Browser
+ Comment=Tor Browser is +1 for privacy and −1 for mass surveillance
+ Categories=Network;WebBrowser;Security;
+ Exec=$HOME/tor-browser_en-US/Browser/start-tor-browser
+ X-TorBrowser-ExecShell=./Browser/start-tor-browser --detach
+ Icon=/usr/share/icons/tor.png
+ StartupWMClass=Tor Browser" > ~/.local/share/applications/tor.desktop
 
+ rm $HOME/.local/share/applications/torinstall.desktop
+ xdg-open https://sourceforge.net/projects/tor-browser-ports/files/ >> /dev/null
+ echo Done
+ sleep 10
 
-read opcion
-case $opcion in
+}
 
-1)
+#Download wine apps and games function.
 
-echo "";
+wineapps () {
 
-read -p "Which branch do you want to use? [s(stable)/u(unstable)]=> " answer
-#stable
-if [ $answer = s ] || [ $answer = stable ]; then
-  echo "Upgrading MFjaro using the stable branch";
-  ignorepkg;
-  sudo pacman-mirrors -aS stable || echo "You are already using the stable branch or the command can't be executed.";
-  sudo pacman -Syyu --ignore=kodi,python-cairosvg,cairosvg || echo "sudo pacman -Syyu - Error";
-  echo;
-  sleep 5;
-  
-  patch;
-  
-  echo;
-  read -p "Clean unused packages and cache? (recommended to free up space after upgrade). [y/n]=> " answer
-  if [ $answer = y ] || [ $answer = Y ]; then
-   echo  "Attention, please read the following warnings before proceeding:";
-   sudo pacman -Scc && paccache -r && sudo pacman -Rns $(pacman -Qtdq);
-  fi
- #unstable
- elif [ $answer = u ] || [ $answer = unstable ]; then
-  echo "Upgrading MFjaro using the unstable branch";
-  ignorepkg;
-  sudo pacman-mirrors -aS unstable || echo "You are already using the unstable branch or the command can't be executed.";
-  sudo pacman -Syyu --ignore=kodi,python-cairosvg,cairosvg  || echo "sudo pacman -Syyu - Error";
-  echo;
-  sleep 5;
-  
-  patch;
-  
-  echo;
-  read -p "Clean unused packages and cache? (recommended to free up space after upgrade.). [y/n]=> " answer
-  if [ $answer = y ] || [ $answer = Y ]; then
-   echo  "Attention, please read the following warnings before proceeding:";
-   sudo pacman -Scc && paccache -r && sudo pacman -Rns $(pacman -Qtdq);
-  fi
-  
-  patch;
-  
+if [ -d "./wineapps" ] 
+then
+    cd wineapps
+    git pull
 else
-  echo "Invalid option.";
+    git clone https://github.com/winuxwinfree/wineapps
+    
 fi
-sleep 5;;
-
-2)
-
-echo "
-Attention, please read the following warnings before proceeding:
-" & sudo pacman -Scc && paccache -r && sudo pacman -Rns $(pacman -Qtdq);
-sleep 5;;
-
-a) 
-
-patch;;
-
-
-b) 
-
-wineapps;;
-
-c)
-
-audiofix;;
-
-d)
-
-addapps;;
-
-q) 
-
-echo "Done, closing. Not working properly? Fenix Linux/OS chat: https://discord.gg/dwAYC5QRDP.";
-sleep 3; exit 1;;
-
-*)
-
-echo "$opc invalid option ";
-echo "Press a key to continue.";
-read foo;;
-
- esac
-done
+echo "Open it from wine explorer. Wine explorer path: My Documents/wineapps."
+sleep 10
 }
 
-ignorepkg () {
-
-#ignore pkg
-
-sudo sed -i ':a;N;$!ba; s/#IgnorePkg   =/IgnorePkg   =*cairosvg*/1' /etc/pacman.conf || echo "Error replacing #IgnorePkg with IgnorePkg... in /etc/pacman.conf"
-sudo sed -i ':a;N;$!ba; s/#IgnorePkg   =/IgnorePkg   =kodi*/1' /etc/pacman.conf || echo "Error replacing #IgnorePkg with IgnorePkg... in /etc/pacman.conf"
-
-}
 
 
 addapps
